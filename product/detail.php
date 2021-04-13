41d664312ff58fce7d86d83aed15e6bc4088101b
Võ Hoài Nam <vhnvohoainam@gmail.com>
2021-04-13 15:08:37 +0700

    fix

--- product/detail.php
+++ product/detail.php
@@ -1,3 +1,6 @@
+<?php 
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="vn">
     <head>
@@ -82,86 +85,9 @@
         <button class="scrollbutton" style="z-index:99;border-radius:1rem;display:none;position: fixed; right: 1rem;bottom: 2rem;padding: 1.2rem 1.4rem;background:#D70018;outline: none;border: none;cursor:pointer">
             <i class="fas fa-sort-up" style="font-size:1.6rem;color:white;transform:translateY(45%);"></i>
         </button>
-        <div class="header">
-            <div class="container">
-                <div class="header__main">
-                    <a href="./index.html" class="header__main-logo">
-                        <!-- <img src="./assets/image/logo.png" alt=""> -->
-                        <h1>TechShop</h1>
-                        <h1 class="mobile-logo">T</h1>
-                    </a>
-                    <form action="" class="header__main-search">
-                        <input type="text" class="search__input" placeholder="Bạn tìm gì...">
-                        <button type="submit" class="search__button">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </form>
-                    <div class="header__main-mini">
-                        <a href="" class="mini__cart">
-                            <i class="fas fa-shopping-cart"></i>
-                            <span>Giỏ hàng</span>
-                        </a>
-                        <a href="" class="mini__history">
-                            <span>LỊCH SỬ <br/>MUA HÀNG</span>
-                        </a>
-                        <a href="" class="mini__history">
-                            <span>ĐĂNG NHẬP</span>
-                        </a>
-                        <a href="" class="mini__history">
-                            <span>Xin chào <br/>Võ Hoài Nam</span>
-                        </a>
-                    </div>
-                </div>
-                <div class="header__nav">
-                    <div class="header__nav-item">
-                        <a href="./phone.html" class="item__title">
-                            <i class="item__icon flaticon-phone"></i>
-                            <span>Điện thoại</span>
-                        </a>
-                    </div>
-                    <div class="header__nav-item">
-                        <a href="./laptop.html" class="item__title">
-                            <i class="item__icon flaticon-analytics"></i>
-                            <span>Laptop</span>
-                        </a>
-                    </div>
-                    <div class="header__nav-item">
-                        <a href="./tablet.html" class="item__title">
-                            <i class="item__icon fas fa-tablet-alt"></i>
-                            <span>Tablet</span>
-                        </a>
-                    </div>
-                    <div class="header__nav-item">
-                        <a href="#" class="item__title">
-                            <i class="item__icon flaticon-headphones"></i>
-                            <span style="margin-right: 1rem;">Phụ kiện</span>
-                            <i class="fas fa-sort-down" style="font-size: 1.8rem;transform: translateY(-25%);"></i>
-                            <i class="fas fa-sort-up" style="font-size: 1.8rem;transform: translateY(25%);"></i>
-                        </a>
-                        <div class="item__subitem">
-                            <a href="#" class="item__subitem-item">Sạc dự phòng</a>
-                            <a href="#" class="item__subitem-item">Tai nghe</a>
-                            <a href="#" class="item__subitem-item">Bàn phím</a>
-                            <a href="#" class="item__subitem-item">Chuột</a>
-                            <a href="#" class="item__subitem-item">Miếng dán màn hình</a>
-                        </div>
-                    </div>
-                    <div class="header__nav-item">
-                        <a href="./watch.html" class="item__title">
-                            <i class="item__icon flaticon-smartwatch"></i>
-                            <span>Đồng hồ thông minh</span>
-                        </a>
-                        
-                    </div>
-                    <div class="header__nav-item">
-                        <a href="./pc.html" class="item__title">
-                            <i class="item__icon fas fa-desktop"></i>
-                            <span>PC, Máy in</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php
+            include_once('../header.php');
+        ?>
         <div class="detail" style="padding-top:100px;">
             <div class="container">
                 <div class="detail__type">
@@ -350,47 +276,11 @@
                 </div>
             </div>
         </div>
-        <div class="footer">
-            <div class="container">
-                <div class="footer__col">
-                    <a href="#">Lịch sử mua hàng</a>
-                    <a href="#">Tìm hiểu về mua trả góp</a>
-                    <a href="#">Chính sách bảo hành</a>
-                    <a href="#">Chính sách đổi trả</a>
-                </div>
-                <div class="footer__col">
-                    <a href="#">Giới thiệu công ty</a>
-                    <a href="#">Tuyển dụng</a>
-                    <a href="#">Gửi góp ý, khiếu nại</a>
-                    <a href="#">Tìm siêu thị (2.460 shop)</a>
-                </div>
-                <div class="footer__col">
-                    <p>Gọi mua hàng <a href="tel:0354714955">1800.1060</a> (7:30 - 22:00)</p>
-                    <p>Gọi khiếu nại <a href="tel:0354714955">1800.1062</a> (8:00 - 21:30)</p>
-                    <p>Gọi bảo hành <a href="tel:0354714955">1800.1064</a> (8:00 - 21:00)</p>
-                    <p>Kỹ thuật <a href="tel:0354714955">1800.1763</a> (7:30 - 22:00)</p>
-                </div>
-                <div class="footer__col footer__collast">
-                    <div class="footer__col-link">
-                        <a target="_blank" href="https://facebook.com/thegioididongcom" class="linkfb" rel="noopener">
-                            <i class="fab fa-facebook-square"></i>3.5tr
-                        </a>
-                        <a target="_blank" href="https://facebook.com/thegioididongcom" class="linkfb" rel="noopener">
-                            <i class="fab fa-youtube"></i>3.5tr
-                        </a>
-                    </div>
-                    <div class="footer__col-partner">
-                        <label>Nhà thành lập :</label>
-                        <a class="partner__frontend"href="https://www.facebook.com/100009457467356" target="_blank">Võ Hoài Nam</a>
-                        <a class="partner__frontend"href="https://www.facebook.com/100008031519330" target="_blank">Huỳnh Minh Hậu</a>
-                        <a class="partner__frontend"href="https://www.facebook.com/100029417300263" target="_blank">Nguyễn Nhật Linh</a>
-                        <a class="partner__backend"href="https://www.facebook.com/100009169309130" target="_blank">Huỳnh Thanh Phong</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php
+            include_once('../footer.php');
+        ?>
         <script>
-            document.write('\u003c\u0064\u0069\u0076\u0020\u0063\u006c\u0061\u0073\u0073\u003d\u0022\u0061\u0066\u0074\u0065\u0072\u0066\u006f\u006f\u0074\u0065\u0072\u0022\u0020\u0073\u0074\u0079\u006c\u0065\u003d\u0022\u0070\u0061\u0064\u0064\u0069\u006e\u0067\u003a\u0031\u0072\u0065\u006d\u003b\u0074\u0065\u0078\u0074\u002d\u0061\u006c\u0069\u0067\u006e\u003a\u0020\u0063\u0065\u006e\u0074\u0065\u0072\u003b\u0066\u006f\u006e\u0074\u002d\u0073\u0069\u007a\u0065\u003a\u0020\u0031\u0030\u0070\u0078\u003b\u0063\u006f\u006c\u006f\u0072\u003a\u0023\u0039\u0039\u0039\u003b\u0066\u006f\u006e\u0074\u002d\u0066\u0061\u006d\u0069\u006c\u0079\u003a\u0020\u0041\u0072\u0069\u0061\u006c\u002c\u0020\u0048\u0065\u006c\u0076\u0065\u0074\u0069\u0063\u0061\u002c\u0020\u0073\u0061\u006e\u0073\u002d\u0073\u0065\u0072\u0069\u0066\u003b\u0022\u003e\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u00a9\u0020\u0032\u0030\u0032\u0031\u002e\u0020\u0043\u00f4\u006e\u0067\u0020\u0074\u0079\u0020\u0063\u1ed5\u0020\u0070\u0068\u1ea7\u006e\u0020\u0054\u0065\u0063\u0068\u0053\u0068\u006f\u0070\u002e\u0020\u0110\u0069\u1ec7\u006e\u0020\u0074\u0068\u006f\u1ea1\u0069\u003a\u0020\u0030\u0033\u0035\u0034\u0020\u0037\u0031\u0034\u0020\u0039\u0035\u0035\u002e\u0020\u0045\u006d\u0061\u0069\u006c\u003a\u0020\u0076\u0068\u006e\u0076\u006f\u0068\u006f\u0061\u0069\u006e\u0061\u006d\u0040\u0067\u006d\u0061\u0069\u006c\u002e\u0063\u006f\u006d\u002e\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u003c\u0062\u0072\u003e\u0043\u0068\u1ecb\u0075\u0020\u0074\u0072\u00e1\u0063\u0068\u0020\u006e\u0068\u0069\u1ec7\u006d\u0020\u006e\u1ed9\u0069\u0020\u0064\u0075\u006e\u0067\u003a\u0020\u0056\u00f5\u0020\u0048\u006f\u00e0\u0069\u0020\u004e\u0061\u006d\u002e\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u003c\u002f\u0064\u0069\u0076\u003e')
+            // document.write('\u003c\u0064\u0069\u0076\u0020\u0063\u006c\u0061\u0073\u0073\u003d\u0022\u0061\u0066\u0074\u0065\u0072\u0066\u006f\u006f\u0074\u0065\u0072\u0022\u0020\u0073\u0074\u0079\u006c\u0065\u003d\u0022\u0070\u0061\u0064\u0064\u0069\u006e\u0067\u003a\u0031\u0072\u0065\u006d\u003b\u0074\u0065\u0078\u0074\u002d\u0061\u006c\u0069\u0067\u006e\u003a\u0020\u0063\u0065\u006e\u0074\u0065\u0072\u003b\u0066\u006f\u006e\u0074\u002d\u0073\u0069\u007a\u0065\u003a\u0020\u0031\u0030\u0070\u0078\u003b\u0063\u006f\u006c\u006f\u0072\u003a\u0023\u0039\u0039\u0039\u003b\u0066\u006f\u006e\u0074\u002d\u0066\u0061\u006d\u0069\u006c\u0079\u003a\u0020\u0041\u0072\u0069\u0061\u006c\u002c\u0020\u0048\u0065\u006c\u0076\u0065\u0074\u0069\u0063\u0061\u002c\u0020\u0073\u0061\u006e\u0073\u002d\u0073\u0065\u0072\u0069\u0066\u003b\u0022\u003e\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u00a9\u0020\u0032\u0030\u0032\u0031\u002e\u0020\u0043\u00f4\u006e\u0067\u0020\u0074\u0079\u0020\u0063\u1ed5\u0020\u0070\u0068\u1ea7\u006e\u0020\u0054\u0065\u0063\u0068\u0053\u0068\u006f\u0070\u002e\u0020\u0110\u0069\u1ec7\u006e\u0020\u0074\u0068\u006f\u1ea1\u0069\u003a\u0020\u0030\u0033\u0035\u0034\u0020\u0037\u0031\u0034\u0020\u0039\u0035\u0035\u002e\u0020\u0045\u006d\u0061\u0069\u006c\u003a\u0020\u0076\u0068\u006e\u0076\u006f\u0068\u006f\u0061\u0069\u006e\u0061\u006d\u0040\u0067\u006d\u0061\u0069\u006c\u002e\u0063\u006f\u006d\u002e\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u003c\u0062\u0072\u003e\u0043\u0068\u1ecb\u0075\u0020\u0074\u0072\u00e1\u0063\u0068\u0020\u006e\u0068\u0069\u1ec7\u006d\u0020\u006e\u1ed9\u0069\u0020\u0064\u0075\u006e\u0067\u003a\u0020\u0056\u00f5\u0020\u0048\u006f\u00e0\u0069\u0020\u004e\u0061\u006d\u002e\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u003c\u002f\u0064\u0069\u0076\u003e')
             window.addEventListener('DOMContentLoaded',function () {
                 imageZoom("myimage", "myresult");
                 paragraph = document.querySelector('.paragraph');
--- product/watch.php
+++ product/watch.php
@@ -1,3 +1,6 @@
+<?php 
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="vn">
   <head>
@@ -17,134 +20,57 @@
     <link rel="stylesheet" type="text/css" href="../assets/icon/flaticon.css" />
   </head>
   <body>
-    <div class="header">
-      <div class="container">
-          <div class="header__main">
-              <a href="./index.html" class="header__main-logo">
-                  <!-- <img src="./assets/image/logo.png" alt=""> -->
-                  <h1>TechShop</h1>
-                  <h1 class="mobile-logo">T</h1>
-              </a>
-              <form action="" class="header__main-search">
-                  <input type="text" class="search__input" placeholder="Bạn tìm gì...">
-                  <button type="submit" class="search__button">
-                      <i class="fas fa-search"></i>
-                  </button>
-              </form>
-              <div class="header__main-mini">
-                  <a href="" class="mini__cart">
-                      <i class="fas fa-shopping-cart"></i>
-                      <span>Giỏ hàng</span>
-                  </a>
-                  <a href="" class="mini__history">
-                      <span>LỊCH SỬ <br/>MUA HÀNG</span>
-                  </a>
-                  <a href="" class="mini__history">
-                      <span>ĐĂNG NHẬP</span>
-                  </a>
-                  <a href="" class="mini__history">
-                      <span>Xin chào <br/>Võ Hoài Nam</span>
-                  </a>
-              </div>
-          </div>
-          <div class="header__nav">
-              <div class="header__nav-item">
-                  <a href="./phone.html" class="item__title">
-                      <i class="item__icon flaticon-phone"></i>
-                      <span>Điện thoại</span>
-                  </a>
-              </div>
-              <div class="header__nav-item">
-                  <a href="./laptop.html" class="item__title">
-                      <i class="item__icon flaticon-analytics"></i>
-                      <span>Laptop</span>
-                  </a>
-              </div>
-              <div class="header__nav-item">
-                  <a href="./tablet.html" class="item__title">
-                      <i class="item__icon fas fa-tablet-alt"></i>
-                      <span>Tablet</span>
-                  </a>
-              </div>
-              <div class="header__nav-item">
-                  <a href="#" class="item__title">
-                      <i class="item__icon flaticon-headphones"></i>
-                      <span style="margin-right: 1rem;">Phụ kiện</span>
-                      <i class="fas fa-sort-down" style="font-size: 1.8rem;transform: translateY(-25%);"></i>
-                      <i class="fas fa-sort-up" style="font-size: 1.8rem;transform: translateY(25%);"></i>
-                  </a>
-                  <div class="item__subitem">
-                      <a href="#" class="item__subitem-item">Sạc dự phòng</a>
-                      <a href="#" class="item__subitem-item">Tai nghe</a>
-                      <a href="#" class="item__subitem-item">Bàn phím</a>
-                      <a href="#" class="item__subitem-item">Chuột</a>
-                      <a href="#" class="item__subitem-item">Miếng dán màn hình</a>
-                  </div>
-              </div>
-              <div class="header__nav-item">
-                  <a href="./watch.html" class="item__title">
-                      <i class="item__icon flaticon-smartwatch"></i>
-                      <span>Đồng hồ thông minh</span>
-                  </a>
-                  
-              </div>
-              <div class="header__nav-item">
-                  <a href="./pc.html" class="item__title">
-                      <i class="item__icon fas fa-desktop"></i>
-                      <span>PC, Máy in</span>
-                  </a>
-              </div>
-          </div>
-      </div>
-    </div>
+  <?php
+            include_once('../header.php');
+        ?>
     <div class="brand">
       <div class="container">
         <a href="" class="brand__item">
           <img
             class="brand__item-img"
-            src="./assets/image/nhanhieu/Apple7077-b_26.jpg"
+            src="../assets/image/nhanhieu/Apple7077-b_26.jpg"
             alt=""
           />
         </a>
         <a href="" class="brand__item">
           <img
             class="brand__item-img"
-            src="./assets/image/nhanhieu/samsung.jpg"
+            src="../assets/image/nhanhieu/samsung.jpg"
             alt=""
           />
         </a>
         <a href="" class="brand__item">
           <img
             class="brand__item-img"
-            src="./assets/image/nhanhieu/Huawei522-b_4.jpg"
+            src="../assets/image/nhanhieu/Huawei522-b_4.jpg"
             alt=""
           />
         </a>
         <a href="" class="brand__item">
           <img
             class="brand__item-img"
-            src="./assets/image/nhanhieu/oppo.jpg"
+            src="../assets/image/nhanhieu/oppo.jpg"
             alt=""
           />
         </a>
         <a href="" class="brand__item">
           <img
             class="brand__item-img"
-            src="./assets/image/nhanhieu/Garmin.png"
+            src="../assets/image/nhanhieu/Garmin.png"
             alt=""
           />
         </a>
         <a href="" class="brand__item">
           <img
             class="brand__item-img"
-            src="./assets/image/nhanhieu/Xiaomi.jpg"
+            src="../assets/image/nhanhieu/Xiaomi.jpg"
             alt=""
           />
         </a>
         <a href="" class="brand__item">
           <img
             class="brand__item-img"
-            src="./assets/image/nhanhieu/Huami-Amazfi.jpg"
+            src="../assets/image/nhanhieu/Huami-Amazfi.jpg"
             alt=""
           />
         </a>
@@ -477,94 +403,8 @@
         </div>
       </div>
     </div>
-    <div class="footer">
-      <div class="container">
-        <div class="footer__col">
-          <a href="#">Lịch sử mua hàng</a>
-          <a href="#">Tìm hiểu về mua trả góp</a>
-          <a href="#">Chính sách bảo hành</a>
-          <a href="#">Chính sách đổi trả</a>
-        </div>
-        <div class="footer__col">
-          <a href="#">Giới thiệu công ty</a>
-          <a href="#">Tuyển dụng</a>
-          <a href="#">Gửi góp ý, khiếu nại</a>
-          <a href="#">Tìm siêu thị (2.460 shop)</a>
-        </div>
-        <div class="footer__col">
-          <p>
-            Gọi mua hàng <a href="tel:0354714955">1800.1060</a> (7:30 - 22:00)
-          </p>
-          <p>
-            Gọi khiếu nại <a href="tel:0354714955">1800.1062</a> (8:00 - 21:30)
-          </p>
-          <p>
-            Gọi bảo hành <a href="tel:0354714955">1800.1064</a> (8:00 - 21:00)
-          </p>
-          <p>Kỹ thuật <a href="tel:0354714955">1800.1763</a> (7:30 - 22:00)</p>
-        </div>
-        <div class="footer__col footer__collast">
-          <div class="footer__col-link">
-            <a
-              target="_blank"
-              href="https://facebook.com/thegioididongcom"
-              class="linkfb"
-              rel="noopener"
-            >
-              <i class="fab fa-facebook-square"></i>3.5tr
-            </a>
-            <a
-              target="_blank"
-              href="https://facebook.com/thegioididongcom"
-              class="linkfb"
-              rel="noopener"
-            >
-              <i class="fab fa-youtube"></i>3.5tr
-            </a>
-          </div>
-          <div class="footer__col-partner">
-            <label>Nhà thành lập :</label>
-            <a
-              class="partner__frontend"
-              href="https://www.facebook.com/100009457467356"
-              target="_blank"
-              >Võ Hoài Nam</a
-            >
-            <a
-              class="partner__frontend"
-              href="https://www.facebook.com/100008031519330"
-              target="_blank"
-              >Huỳnh Minh Hậu</a
-            >
-            <a
-              class="partner__frontend"
-              href="https://www.facebook.com/100029417300263"
-              target="_blank"
-              >Nguyễn Nhật Linh</a
-            >
-            <a
-              class="partner__backend"
-              href="https://www.facebook.com/100009169309130"
-              target="_blank"
-              >Huỳnh Thanh Phong</a
-            >
-          </div>
-        </div>
-      </div>
-    </div>
-    <div
-      class="afterfooter"
-      style="
-        padding: 1rem;
-        text-align: center;
-        font-size: 10px;
-        color: #999;
-        font-family: Arial, Helvetica, sans-serif;
-      "
-    >
-      © 2021. Công ty cổ phần TechShop. Điện thoại: 0354 714 955. Email:
-      vhnvohoainam@gmail.com.
-      <br />Chịu trách nhiệm nội dung: Võ Hoài Nam.
-    </div>
+    <?php
+            include_once('../footer.php');
+        ?>
   </body>
 </html>
